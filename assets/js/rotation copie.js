c1e03c07a744b028ec78be6c94b7f9411516cc9f
Capucine Frx <52498069+CapucineFX@users.noreply.github.com>
2026-04-17 15:11:23 +0200

    no timer

--- assets/js/rotation copie.js
+++ assets/js/rotation copie.js
@@ -3,10 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const carouselElement = document.querySelector('#bannerCarousel');
 
   const carousel = new bootstrap.Carousel(carouselElement, {
-    interval: 5000,     // durée entre les slides (5s)
-    ride: 'carousel',   // autoplay
-    pause: false,       // ne s'arrête pas au hover
-    wrap: true,         // boucle infinie
+
     touch: true         // swipe mobile
   });
 
